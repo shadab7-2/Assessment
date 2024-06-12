90aee4f3011ecab6625a4100d53f5269a00ddced
Sahdab Ansari <sahdabansari972@gmail.com>
2024-06-12 20:28:27 +0530

    final answer2

--- que2.js
+++ que2.js
@@ -1,15 +1,17 @@
 function printStar(){
     for(let i = 0; i < 3; i++){
+        let star = ""
         for(let j = 0 ; j <= i; j++){
-            console.log(" * ")
-        }  
-        // if(i = 1){
-        //     console.log(" * ")
-        // }   else if( i = 2){
-        //     console.log(" * * ")
-        // }else{
-        //     console.log(" * * * ")
-        // }
-    }
+            star += "*"
+            }  
+            // if(i = 1){
+                //     console.log(" * ")
+                // }   else if( i = 2){
+                    //     console.log(" * * ")
+                    // }else{
+                        //     console.log(" * * * ")
+                        // }
+                    console.log(star)
+                        }
 }
 console.log(printStar())
